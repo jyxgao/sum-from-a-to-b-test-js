be2789031834031adb81091ff2966b124d1c8352
Jenny Gao <gao.jenny9@gmail.com>
2020-07-25 04:05:30 +0000

    implemented recursive function sum a to b

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
-function sum(fromN, toN) {
+/* Function that sums a range of numbers - assuming a is smaller than toN */
+const sum = function(a, toN) {
+  if (a === toN) {
+    return a;
+  }
   // Sum all the values from fromN up to toN
+  return a + sum(a+1, toN)
 }
 
 module.exports = sum;
